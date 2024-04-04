60852d3d5f1866abf05800521caa74758d591df9
osvaldsoza <osvald.soza@gmail.com>
2024-04-04 18:32:59 -0300

    Initial Commit

---
+++ BottomSheetSwiftUI/BottomSheetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheetSwiftUIApp.swift
+//  BottomSheetSwiftUI
+//
+//  Created by Osvaldo Souza Ferreira on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
